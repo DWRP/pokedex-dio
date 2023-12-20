c377b7539ac544c0bc1152eb0702050766c5471f
Douglas Pardim <douglas.skd@gmail.com>
2023-12-20 14:42:40 -0300

    fix(main.js): changed code generation to fix render error

--- src/js/main.js
+++ src/js/main.js
@@ -5,28 +5,30 @@ const cardComponent = (pokemon) => {
   const photo = pokemonPhoto(pokemon.number, pokemon.photo);
   const body = pokemonBody(pokemon.name, pokemon.number);
   const types = pokemonTypes(pokemon.number, pokemon.types);
-  const card = cardBase(pokemon.number, photo + body + types);
+  const card = cardBase(pokemon.number, [photo, body, types]);
   return card;
 };
 
 const getMorePokemons = async (url) => {
   const pokemons = await service.getAllPokemons(url.replace(service.API, "/"));
 
-  const newHtmlBody = pokemons.pokemons.map(cardComponent).join("");
+  const newHtmlBody = pokemons.pokemons.map(cardComponent);
   const carregarMais = carregarMaisButton(pokemons.next);
- 
-  pokemonList.removeChild(document.getElementById('loadMore'))
 
-  pokemonList.innerHTML = pokemonList.innerHTML + newHtmlBody + (pokemons.next ? carregarMais : "");
+  pokemonList.removeChild(document.getElementById("loadMore"));
+
+  newHtmlBody.forEach((child) => pokemonList.appendChild(child));
+  pokemonList.appendChild(pokemons.next ? carregarMais : "");
 };
 
 async function renderPokemons() {
   const pokemons = await service.getAllPokemons("pokemon?limit=20");
 
-  const newHtmlBody = pokemons.pokemons.map(cardComponent).join("");
+  const newHtmlBody = pokemons.pokemons.map(cardComponent);
   const carregarMais = carregarMaisButton(pokemons.next);
 
-  pokemonList.innerHTML = newHtmlBody + (pokemons.next ? carregarMais : "");
+  newHtmlBody.forEach((child) => pokemonList.appendChild(child));
+  pokemonList.appendChild(pokemons.next ? carregarMais : "");
 }
 
 const main = async () => {
